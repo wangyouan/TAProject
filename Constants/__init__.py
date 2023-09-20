15ac42ed6cbbc39487ad694fc7976001c62a7810
Youan Wang <wangyouan0629@hotmail.com>
2023-09-20 14:52:06 +0800

    Initialize

---
+++ Constants/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: __init__.py
+# @Date: 2023/9/20
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TAProject
+This project is used to handle some tax avoidance related projects.
+
+Current project
+* Same-Sex Marriage
+* EDGAR Implementation
